0227af1071b987cbcc1e29693f553762dac2efc2
Rodrigo Santana <se03rodrigo@gmail.com>
2024-05-02 08:33:19 -0300

    correção do problema de abrir duas abas

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import f_qualidade.tratar_dados_qualidade,f_qualidade.planilha_qualidade
 import f_eficiencia.tratar_dados_eficiencia,f_eficiencia.planilha_eficiencia
 import f_vazamentos.tratar_dados_vazamentos
 import webbrowser
+import keyboard
 
 webbrowser.open('http://127.0.0.1:5000')
+keyboard.press_and_release('ctrl+w')
 ALLOWED_EXTENSIONS = {'xlsx','zip'}
 UPLOAD_FOLDER = 'arquivos'
 
